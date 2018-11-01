672cea79cca4a0a15da7e320c0672fdc9ea35db8
ImportProgram <ImportProgram@users.noreply.github.com>
2018-10-31 23:52:39 -0400

    Add an API

---
+++ example/api/example/something.js
@@ -0,0 +1,17 @@
+/**
+ * Example
+ * @author ImportProgram
+ */
+
+var event = new EventEmitter();
+
+Observo.onCustomMount((imports) => {
+   
+})
+Observo.register(null, {
+    GLOBAL: {
+        doSomething: () => {
+
+        }
+    }
+})
